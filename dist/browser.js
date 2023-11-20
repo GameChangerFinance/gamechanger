88e1cda02ac13318d75e9512b08fe5bbd1a206a5
zxpectre <15870846+zxpectre@users.noreply.github.com>
2023-11-20 18:13:23 -0300

    changed npm package name

--- dist/browser.js
+++ dist/browser.js
@@ -4658,7 +4658,7 @@ const deflate_slow = (s, flush) => {
         s.match_length <= 5 &&
         (s.strategy === Z_FILTERED ||
           (s.match_length === MIN_MATCH &&
-            s.strstart - s.match_start > 4096)) /*TOO_FAR*/
+            s.strstart - s.match_start > 4096) /*TOO_FAR*/)
       ) {
         /* If prev_match is also MIN_MATCH, match_start is garbage
          * but we will ignore the current match anyway.
