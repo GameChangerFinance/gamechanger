9e498afa3bcae99aa439c2df4fabc25c6220de9d
zxpectre <15870846+zxpectre@users.noreply.github.com>
2026-03-18 12:36:30 -0300

    feat: examples are now automatically built. Some cleanup

--- examples copy/res/browser.js
+++
@@ -1,2 +0,0 @@
-export { default } from './browser.runtime.js'
-export { default as gc } from './browser.runtime.js'
--- examples copy/res/nodejs.js
+++
@@ -1,5 +0,0 @@
-import gc from './nodejs.cjs'
-
-export const { encode, snippet, encodings, utils, config } = gc
-export { gc }
-export default gc
